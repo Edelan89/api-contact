c2559b38bf2639b0b6fad17d53399916dd1a65da
Edelan <maximilianolf@hotmail.com>
2023-06-17 09:14:01 +0300

    Rutas Creadas

--- app.py
+++ app.py
@@ -4,3 +4,34 @@ from flask_sqlalchemy import SQLAlchemy
 app = Flask(__name__)
 app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///contacts.db'
 db = SQLAlchemy(app)
+
+# Crear modelo de base de datos
+
+class Contact(db.Model):
+    id = db.Column(db.Integer, primary_key = True)
+    name = db.Column(db.String(50), nullable = False)
+    email = db.Column(db.String(50), nullable = False)
+    phone = db.Column(db.String(11), nullable = False)
+
+    def serialize(self):
+        return {
+            'id': self.id,
+            'name': self.name,
+            'email': self.email,
+            'phone': self.phone
+        }
+
+#Crear las tablas de base de datos
+
+with app.app_context():
+    db.create_all()
+
+#Crear Rutas
+
+@app.route('/contacts', methods= ['GET'])
+def get_contacts():
+    return 'Lista de contactos'
+
+@app.route('/contacts', methods= ['POST'])
+def create_contact():
+    return 'Se creo un contacto'
